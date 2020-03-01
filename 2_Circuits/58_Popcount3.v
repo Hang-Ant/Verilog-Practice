79794189dff603c0506b3e174a602ce3ba05326d
Hang-Ant <3220531906@qq.com>
2020-03-01 20:59:22 +0800

    add Bugs and Solutions

--- 2_Circuits/58_Popcount3.v
+++ 2_Circuits/58_Popcount3.v
@@ -1,15 +1,26 @@
-module top_module( 
+module test_module( 
     input [2:0] in,
-    output [1:0] out );
+    output reg [1:0] out );
     
     integer i;
     
-    always @(*) begin
+    always @(*) 
+	begin
         out = 0;
-        for(i = 0; i < 3; i ++) begin
+        for(i = 0; i < 3; i = i + 1) 
+		begin
             if(in[i])
                 out = out + 1;
         end
     end
-
 endmodule
+/*
+Bugs and solutions:
+1.  if the third line is written as : output [1:0] out);
+	Will get this error : Verilog HDL Procedural Assignment
+	error at test_module.v(9): object "out" on left-hand 
+	side of assignment must have a variable data type.
+2.  If the tenth line is written as : ...;i++)
+	will get this error : Ignored design unit "test_module"
+	at test_module.v(1) due to previous errors
+*/
